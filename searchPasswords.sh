f901dba5ba2304b33fc74b52c89de69027252b2a
andrewdscott12 <andrew@andrewdscott.com>
2022-12-23 13:34:49 -0500

    Initial commit Utility filters files owned by root and daemon

---
+++ searchPasswords.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+for FILESYSTEM in $(mount | grep -E 'ext|xfs|jfs|btrfs|mmfs' | awk '{print $3}')
+do
+  echo "Filesystem $FILESYSTEM"
+  IFS=$'\n'
+  for i in $(find $FILESYSTEM -xdev -type f -perm -o+r ! -user root ! -user daemon )
+  do 
+    file $i | grep "ASCII text"  > /dev/null
+    if [ $? -eq 0 ]
+    then
+      cat $i | tr '[ <>,:\[\]\9\0]' '\n' | grep -P '\b(?=.\d)(?=.*[a-z])(?=.*[-\#\$\.\%\&\*])\b.{8}' > /dev/null
+      if [ $? -eq 0 ] 
+      then 
+        UNAME=$(stat --format '%U' "$i")
+        echo "Possible password(s) found in $i owned by $UNAME"
+      fi
+    fi
+  done
+done
